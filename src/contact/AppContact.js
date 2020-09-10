580066a0d695382a9eefd9c614cda01c5c5588cf
HTarzwell <htarzwell@gmail.com>
2020-09-10 13:14:46 -0400

    updated project icons

--- src/contact/AppContact.js
+++ src/contact/AppContact.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeBranch, faMailBulk, faSms } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function AppContact() {
     return (
-        <div className="App-contact">
-        <p>placeholder 1</p>
+      <div className="App-contact">
+        <FontAwesomeIcon icon={faCodeBranch}/>
+        <FontAwesomeIcon icon={faLinkedin}/>
+        <FontAwesomeIcon icon={faMailBulk}/>
+        <FontAwesomeIcon icon={faSms}/>
       </div>
     )
 }
