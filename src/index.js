54c6f62f5f4155b31603efefee896c85cdf3c2aa
HTarzwell <htarzwell@gmail.com>
2020-09-28 17:08:41 -0400

    fixed package management error

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider } from "@apollo/client";
+import { ApolloProvider as ApolloHooksProvider } from "@apollo/client";
 import { HttpLink } from 'apollo-link-http';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -19,7 +19,9 @@ const client = new ApolloClient({
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-       <App />
+      <ApolloHooksProvider client={client}>
+        <App />
+      </ApolloHooksProvider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
