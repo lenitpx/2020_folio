84c306ffb8332d47260047b17e013b5421b06bbd
HTarzwell <htarzwell@gmail.com>
2021-01-25 11:41:31 -0500

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import AppHeader from './header/AppHeader.js'
 import AppContact from './contact/AppContact.js'
 import AppCount from './count/AppCount.js'
-import AppEvolution from './evolution/AppEvolution.js'
+import AppWork from './evolution/AppWork.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <AppHeader />
+      <header className="App-header">
+        <div className="titleSplash">beansoft</div>
+        <div className="menuItem">work</div>
+        <div className="menuItem">background</div>
+        <div className="menuItem">contact</div>
+      </header>
     </div>
   );
 }
