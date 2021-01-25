28372d78f53556d420cf60ca2c19509aee764574
HTarzwell <htarzwell@gmail.com>
2021-01-25 13:41:16 -0500

    add folder plus mobile

---
+++ src/evolution/AppWork.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppWork() {
+    return (
+        <div className="App-work">
+        <p>placeholder 3</p>
+      </div>
+    )
+}
+
+export default AppWork;
